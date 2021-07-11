6e0bad09e1b1d52bf88e4d134258f30b8bc49a78
Bianca Barbosa <yume_shiro@hotmail.com>
2021-07-10 22:17:24 -0300

    feat: creating logic to open and close modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,3 +58,27 @@ skillsHeader.forEach((el) => {
 })
 
 /* Qualification Tabs */
+
+
+/* Services Modal */
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
