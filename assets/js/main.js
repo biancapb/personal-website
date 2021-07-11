3670300fa037cc40f453f3223cf9b84fa21a96fb
Bianca Barbosa <yume_shiro@hotmail.com>
2021-07-11 20:19:44 -0300

    feat: creating swiper portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,3 +82,17 @@ modalCloses.forEach((modalClose) => {
         })
     })
 })
+
+/* Portfolio Swiper */
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
