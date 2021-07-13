31430319406878d88d065e459bbfe23135d8acc0
Bianca Barbosa <yume_shiro@hotmail.com>
2021-07-12 21:39:07 -0300

    feat: creating scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,3 +136,11 @@ function scrollHeader() {
         
 }
 window.addEventListener('scroll', scrollHeader)
+
+/* Show scroll up */
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up')
+
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
