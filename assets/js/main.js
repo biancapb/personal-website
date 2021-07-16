5ecd56813c7b47e9b7de1151aad06d0750fecf17
Bianca Barbosa <yume_shiro@hotmail.com>
2021-07-16 00:47:14 -0300

    feat: creating responsive website

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,7 +72,25 @@ skillsHeader.forEach((el) => {
 })
 
 /* Qualification Tabs */
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
 
+    tabs.forEach(tab =>{
+        tab.addEventListener('click', () =>{
+            const target = document.querySelector(tab.dataset.target)
+
+            tabContents.forEach(tabContent =>{
+                tabContent.classList.remove('qualification__active')
+            })
+            target.classList.add('qualification__active')
+
+            tab.forEach(tab => {
+                tab.classList.remove('qualification__active')
+            })
+
+            tab.classList.add('qualification__active')
+        })
+    })
 
 /* Services Modal */
 const modalViews = document.querySelectorAll('.services__modal'),
